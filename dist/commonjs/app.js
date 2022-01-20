308187e9c4a3e3b90875fef7407ee6ca072cc1c2
hj <eowr22@gmai.com>
2022-01-20 15:11:22 +0900

    Build with Babel & Add dist folder for saving the result

---
+++ dist/commonjs/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var React = require('react');
+
+module.exports = function App() {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello World");
+};
---
+++ dist/commonjs/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var React = require('react');
+
+var ReactDOM = require('react-dom');
+
+var App = require('./app');
+
+ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)), document.getElementById("root"));
